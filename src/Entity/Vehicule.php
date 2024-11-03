1817fc6baf4f28b2bb5b9c19cfa9db08b1418193
isabella <isabella.kafikaila04@gmail.com>
2024-11-04 01:55:27 +1100

    feat/feature: add entity et repository, migration

---
+++ src/Entity/Vehicule.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VehiculeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VehiculeRepository::class)]
+class Vehicule
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $immatriculation = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateMiseEnCirculation = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateRentree = null;
+
+    #[ORM\Column]
+    private ?int $chevauxFiscaux = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\ManyToOne(inversedBy: 'vehicules')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Modele $modele = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImmatriculation(): ?string
+    {
+        return $this->immatriculation;
+    }
+
+    public function setImmatriculation(string $immatriculation): static
+    {
+        $this->immatriculation = $immatriculation;
+
+        return $this;
+    }
+
+    public function getDateMiseEnCirculation(): ?\DateTimeInterface
+    {
+        return $this->dateMiseEnCirculation;
+    }
+
+    public function setDateMiseEnCirculation(\DateTimeInterface $dateMiseEnCirculation): static
+    {
+        $this->dateMiseEnCirculation = $dateMiseEnCirculation;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getDateRentree(): ?\DateTimeInterface
+    {
+        return $this->dateRentree;
+    }
+
+    public function setDateRentree(\DateTimeInterface $dateRentree): static
+    {
+        $this->dateRentree = $dateRentree;
+
+        return $this;
+    }
+
+    public function getChevauxFiscaux(): ?int
+    {
+        return $this->chevauxFiscaux;
+    }
+
+    public function setChevauxFiscaux(int $chevauxFiscaux): static
+    {
+        $this->chevauxFiscaux = $chevauxFiscaux;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getModele(): ?Modele
+    {
+        return $this->modele;
+    }
+
+    public function setModele(?Modele $modele): static
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+}
