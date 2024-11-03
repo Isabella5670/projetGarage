dcc3d73fd37275b8ad4797feda3a24e8f0afa588
isabella <isabella.kafikaila04@gmail.com>
2024-11-03 18:23:21 +1100

    init symfony

---
+++ src/Controller/CatalogueController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CatalogueController extends AbstractController
+{
+    #[Route('/catalogue', name: 'app_catalogue')]
+    public function index(): Response
+    {
+        return $this->render('catalogue/index.html.twig', [
+            'controller_name' => 'CatalogueController',
+        ]);
+    }
+
+    #[Route('/catalogue/12', name: 'catalogue_show')]
+    public function show() {
+        return $this->render('catalogue/show.html.twig');
+    }
+}
